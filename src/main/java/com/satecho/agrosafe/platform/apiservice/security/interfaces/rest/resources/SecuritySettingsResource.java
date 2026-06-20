0dc23a339048aca9091b8f350ef215f7adfed16f
Mitos20 <a20181159@pucp.edu.pe>
2026-06-19 19:54:46 -0500

    feat(security): add SecuritySettingsResource and assembler for transforming SecuritySettings entities

---
+++ src/main/java/com/satecho/agrosafe/platform/apiservice/security/interfaces/rest/resources/SecuritySettingsResource.java
@@ -0,0 +1,5 @@
+package com.satecho.agrosafe.platform.apiservice.security.interfaces.rest.resources;
+
+public record SecuritySettingsResource(Long id, Long farmId, Integer motionSensitivity, String alertMode,
+                                       String detectionScheduleStart, String detectionScheduleEnd,
+                                       String notificationContacts) {}
