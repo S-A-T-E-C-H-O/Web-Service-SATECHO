6152ef01cdbcd09027c6ad04597566a30adca237
B-Gamio <gamiobrenda@gmail.com>
2026-06-21 12:50:11 -0500

    feat(tests): update imports in test classes to use apiservice package

--- src/test/java/com/satecho/agrosafe/TestApplication.java
+++ src/test/java/com/satecho/agrosafe/TestApplication.java
@@ -1,12 +1,12 @@
 package com.satecho.agrosafe;
 
-import com.satecho.agrosafe.platform.Application;
+import com.satecho.agrosafe.platform.apiservice.BackApiApplication;
 import org.springframework.boot.SpringApplication;
 
 public class TestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.from(Application::main).with(TestcontainersConfiguration.class).run(args);
+		SpringApplication.from(BackApiApplication::main).with(TestcontainersConfiguration.class).run(args);
 	}
 
 }
